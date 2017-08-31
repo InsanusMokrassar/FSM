358030896334943bee952ad09824f1aff0e4b153
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-08-31 22:32:08 +0600

    add handling correct exception

--- src/main/kotlin/com/github/insanusmokrassar/FSM/core/Runner.kt
+++ src/main/kotlin/com/github/insanusmokrassar/FSM/core/Runner.kt
@@ -9,42 +9,45 @@ class Runner(private val states: List<State>, private val firstState: Int = 0) :
         val stack =  Stack<Int>()
         val inputDeque = ArrayDeque<String>(input.toCharArray().map { it.toString() })
         var stateNum = firstState
-        while (inputDeque.isNotEmpty()) {
-            val currentInput = inputDeque.peek()
-            val currentState = states[stateNum]
-            if (currentState.regex.matches(currentInput)) {
-                try {
-                    currentState.action(currentInput)
-                } catch (e: Throwable) {
-                    Logger.getGlobal().throwing(
-                            javaClass.simpleName,
-                            "invoke",
-                            e
-                    )
-                }
-                if (currentState.stack) {
-                    stack.push(stateNum + 1)
-                }
-                stateNum = if (currentState.next != null) {
-                    currentState.next!!
+        try {
+            while (true) {
+                val currentState = states[stateNum]
+                val currentInput = inputDeque.peek()
+                if (currentState.regex.matches(currentInput)) {
+                    if (currentState.accept) {
+                        inputDeque.pop()
+                    }
+                    try {
+                        currentState.action(currentInput)
+                    } catch (e: Throwable) {
+                        Logger.getGlobal().throwing(
+                                javaClass.simpleName,
+                                "invoke",
+                                e
+                        )
+                    }
+                    if (currentState.stack) {
+                        stack.push(stateNum + 1)
+                    }
+                    stateNum = if (currentState.next != null) {
+                        currentState.next!!
+                    } else {
+                        stack.pop()
+                    }
                 } else {
-                    stack.pop()
-                }
-                if (currentState.accept) {
-                    inputDeque.pop()
-                }
-            } else {
-                if (currentState.error) {
-                    throw IllegalArgumentException("Error in read input")
-                } else {
-                    stateNum++
+                    if (currentState.error) {
+                        throw IllegalArgumentException("Error in read input")
+                    } else {
+                        stateNum++
+                    }
                 }
             }
-        }
-        if (stack.isEmpty()) {
-            return
-        } else {
-            throw IllegalArgumentException("Stack is not empty, but symbols is empty.")
+        } catch (e: EmptyStackException) {
+            if (inputDeque.isEmpty()) {
+                return
+            } else {
+                throw IllegalArgumentException("Stack is not empty, but symbols is empty.")
+            }
         }
     }
 }
