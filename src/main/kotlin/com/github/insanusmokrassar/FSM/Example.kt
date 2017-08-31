6a4c2e4ef4207af63e1669fb931850bc08da1e95
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-08-31 22:34:06 +0600

    add example

--- src/main/kotlin/com/github/insanusmokrassar/FSM/Example.kt
+++ src/main/kotlin/com/github/insanusmokrassar/FSM/Example.kt
@@ -1,5 +1,27 @@
 package com.github.insanusmokrassar.FSM
 
-fun main(args: Array<String>) {
+import com.github.insanusmokrassar.FSM.core.*
+import java.util.*
 
+fun main(args: Array<String>) {
+    val runner = Runner(
+            listOf(
+                    ErrorState("\\d", 3),
+                    ClearState("\\d", 5),
+                    ErrorState(";", 7),
+                    StackErrorState("\\d", 1),
+                    AcceptErrorReturnState(";"),
+                    AcceptErrorState("\\d", 6, { println("read $it") }),
+                    ErrorState("[\\d;]", 1),
+                    ErrorReturnState(";")
+            )
+    )
+    val scanner = Scanner(System.`in`)
+    while (true) {
+        try {
+            runner(scanner.nextLine())
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
 }
