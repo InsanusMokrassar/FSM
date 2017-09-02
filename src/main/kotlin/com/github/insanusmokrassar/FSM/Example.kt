63fd63aa246d9c2b47bee984e7aa3942ba7e3588
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-09-02 22:05:18 +0600

    add exanple setting from json

--- src/main/kotlin/com/github/insanusmokrassar/FSM/Example.kt
+++ src/main/kotlin/com/github/insanusmokrassar/FSM/Example.kt
@@ -4,18 +4,26 @@ import com.github.insanusmokrassar.FSM.core.*
 import java.util.*
 
 fun main(args: Array<String>) {
-    val runner = Runner(
-            listOf(
-                    ErrorState("\\d", 3),
-                    ClearState("\\d", 5),
-                    ErrorState(";", 7),
-                    StackErrorState("\\d", 1),
-                    AcceptErrorReturnState(";"),
-                    AcceptErrorState("\\d", 6, { println("read $it") }),
-                    ErrorState("[\\d;]", 1),
-                    ErrorReturnState(";")
-            )
-    )
+
+    val runner = if (args.isNotEmpty()) {
+        args.forEach {
+            println(it)
+        }
+        return
+    } else {
+        Runner(
+                listOf(
+                        ErrorState("\\d", 3),
+                        ClearState("\\d", 5),
+                        ErrorState(";", 7),
+                        StackErrorState("\\d", 1),
+                        AcceptErrorReturnState(";"),
+                        AcceptErrorState("\\d", 6, { println("read $it") }),
+                        ErrorState("[\\d;]", 1),
+                        ErrorReturnState(";")
+                )
+        )
+    }
     val scanner = Scanner(System.`in`)
     while (true) {
         try {
